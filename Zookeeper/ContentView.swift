7304197ab6bb1dd2081f7e09c77c473e6178c8e6
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-04-13 18:33:02 +0100

    First commit

---
+++ Zookeeper/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Zookeeper
+//
+//  Created by Wing Hei Tsui on 11/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        
+    var body: some View {
+        List {
+            ForEach(animalGroups) { animalGroup in
+                Section(header: Text(animalGroup.groupName)) {
+                    ForEach(animalGroup.animals) { animal in
+                        Text(animal.name)
+                    }
+                }
+
+            }
+        }
+    }
+    
+    struct Animal: Identifiable {
+        var id = UUID()
+        var name: String
+    }
+    
+    struct AnimalGroup: Identifiable {
+        var id = UUID()
+        var groupName: String
+        var animals: [Animal]
+    }
+    
+    let animalGroups = [
+        AnimalGroup(groupName: "Pets", animals: [
+            Animal(name: "Dog"),
+            Animal(name: "Cat"),
+            Animal(name: "Parrot"),
+            Animal(name: "Mouse")
+        ]),
+        AnimalGroup(groupName: "Farm", animals: [
+            Animal(name: "Cow"),
+            Animal(name: "Horse"),
+            Animal(name: "Goat"),
+            Animal(name: "Sheep"),
+        ]),
+        AnimalGroup(groupName: "Critters", animals: [
+            Animal(name: "Firefly"),
+            Animal(name: "Spider"),
+            Animal(name: "Ant"),
+            Animal(name: "Squirrel"),
+        ])
+    ]
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Wing Hei Tsui on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
